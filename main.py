53ff5779aca779bf32695efb081caad89b4a3803
Ana Puerma <72268410+apuerma@users.noreply.github.com>
2021-02-16 14:02:27 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def calculate_imc():
+    if request.method == "POST":
+        info = request.form
+        weight = int(info['weight'])
+        height = int(info['height'])
+
+        imc = weight / np.square(height/100)
+        
+        return render_template('show.html', imc = imc)
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
