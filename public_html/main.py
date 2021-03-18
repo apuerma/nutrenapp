3fb6cc5808db41af2e5169b6779d2dafaa381cf2
Ana Puerma <72268410+apuerma@users.noreply.github.com>
2021-03-18 19:10:13 +0100

    Update main.py

--- public_html/main.py
+++ public_html/main.py
@@ -2,6 +2,11 @@ import numpy as np
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html', the_title='NutrenApp')
+
 @app.route('/', methods=["GET","POST"])
 def calculate_imc():
     if request.method == "POST":
